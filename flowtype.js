9c51d7d84531faa9bc578e7093deec0224d4c54e
Steven Truesdell <steven@truesdell.io>
2022-05-27 12:07:35 -0600

    0.5.0-rc2

--- flowtype.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  settings: {
-    flowtype: {
-      onlyFilesWithFlowAnnotation: true,
-    },
-  },
-  plugins: ['flowtype'],
-  extends: ['./rules/flowtype/flowtype'].map(require.resolve),
-};
--- jsx-a11y.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  parserOptions: {
-    ecmaFeatures: {
-      jsx: true,
-    },
-  },
-  plugins: ['jsx-a11y'],
-  extends: ['./rules/jsx-a11y/jsx-a11y'].map(require.resolve),
-};
